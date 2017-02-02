265141022f7959237ec2a3c1428fa1d5f0d9ba75
Tomoatsu Shimada <shimada@walbrix.com>
2017-02-02 10:14:05 +0900

    remove single newline from text extracted from pdf

--- src/extract/pdftotext.py
+++ src/extract/pdftotext.py
@@ -6,8 +6,11 @@ Created on 2014/08/15
 emerge poppler
 '''
 
+import re
 import extract
 
 def do(filename):
     text = extract.process_output(["/usr/bin/pdftotext",filename, "-"])
+    # 単一の改行を削除。1文字ごとに改行されてしまっているような縦書きPDFを与えられてしまった時のため
+    text = re.sub(r'\n(\n*)','\\1', text)
     return ("", extract.utf8_cleanup(text))
